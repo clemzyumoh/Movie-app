ccd6814b09b20265dfd4bcebe5263293bedbb568
clemzyumoh <142527861+clemzyumoh@users.noreply.github.com>
2024-08-29 17:09:42 -0700

    add link to banner play button

--- src/components/bannerHome.js
+++ src/components/bannerHome.js
@@ -82,9 +82,9 @@ const BannerHome = () => {
                     <span>|</span>
                     <p>View : {Number(data.popularity).toFixed(0)} </p>
                   </div>
-                  <button className="bg-white px-4 py-2 text-black font-bold rounded mt-4 mb-3 border-none  hover:bg-gradient-to-l from-red-700 to-orange-500 shadow-md transition-all hover:scale-105">
+                  <Link to={"/"+ data?.media_type+"/"+data?.id} className="bg-white px-4 py-2 text-black font-bold rounded mt-4 mb-3 border-none  hover:bg-gradient-to-l from-red-700 to-orange-500 shadow-md transition-all hover:scale-105">
                     Play Now
-                  </button>
+                  </Link>
                 </div>
               </div>
             </div>
